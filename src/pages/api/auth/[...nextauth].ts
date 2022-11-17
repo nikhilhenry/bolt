61e4d7cf9ca4a505066d247bed59d47d3d00d081
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-11-18 00:45:26 +0530

    prettier

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import CredentialsProvider from "next-auth/providers/credentials";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
@@ -7,24 +7,39 @@ import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
 
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
+    session: {
+        strategy: "jwt",
+        maxAge: 3000,
     },
-  },
-  // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-  ],
+    // Configure one or more authentication providers
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: {
+                    label: "Username",
+                    type: "text",
+                    placeholder: "nikihl",
+                },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(_credentials, _req) {
+                // Add logic here to look up the user from the credentials supplied
+                const user = {
+                    id: "1",
+                    name: "J Smith",
+                    email: "jsmith@example.com",
+                };
+                if (user) {
+                    console.log("should have logged in successfully");
+                    return user;
+                } else {
+                    return null;
+                }
+            },
+        }),
+    ],
 };
 
 export default NextAuth(authOptions);
